e095bd40135d88e673ec0e17d26517caaa9ea338
AmirHosseinHasanloo <AmirHosseinHasanloo2005@gmail.com>
2025-02-15 23:19:16 +0330

    Confiure the sln project

--- RealStateAPI/Controllers/CategoriesController.cs
+++ RealStateAPI/Controllers/CategoriesController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using RealStateAPI.Context;
+using RealStateAPI.Models;
 
 namespace RealStateAPI.Controllers
 {
@@ -7,6 +9,68 @@ namespace RealStateAPI.Controllers
     [ApiController]
     public class CategoriesController : ControllerBase
     {
-        
+        private ApiDbContext _context;
+
+        public CategoriesController(ApiDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/<CategoriesController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_context.Categories.ToList());
+        }
+
+        // GET api/<CategoriesController>/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            return Ok(_context.Categories.Find(id));
+        }
+
+        // POST api/<CategoriesController>
+        [HttpPost]
+        public IActionResult Post([FromBody] Category category)
+        {
+            if (category != null)
+            {
+                _context.Categories.Add(category);
+                _context.SaveChanges();
+                return StatusCode(StatusCodes.Status201Created, "object successfully added.");
+            }
+
+            return StatusCode(StatusCodes.Status400BadRequest);
+        }
+
+        // PUT api/<CategoriesController>/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Category category)
+        {
+            if (_context.Categories.Any(c => c.Id == id))
+            {
+                _context.Categories.Update(category);
+                _context.SaveChanges();
+                return StatusCode(StatusCodes.Status200OK, "Category updated successfully.");
+            }
+
+            return StatusCode(StatusCodes.Status404NotFound);
+        }
+
+        // DELETE api/<CategoriesController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var category = _context.Categories.SingleOrDefault(c => c.Id == id);
+            if (category != null)
+            {
+                _context.Categories.Remove(category);
+                _context.SaveChanges();
+                return StatusCode(StatusCodes.Status200OK, "Category deleted successfully.");
+            }
+
+            return StatusCode(StatusCodes.Status404NotFound);
+        }
     }
 }

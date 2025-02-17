0ec3f4cc1de330d23ba433ceeac788fbe5353de0
AmirHosseinHasanloo <AmirHosseinHasanloo2005@gmail.com>
2025-02-17 18:33:40 +0330

    serilog and newtonsoft added to project.

--- RealStateAPI/Program.cs
+++ RealStateAPI/Program.cs
@@ -1,13 +1,29 @@
+using Microsoft.Build.Framework;
 using Microsoft.EntityFrameworkCore;
 using RealStateAPI.Context;
+using Serilog;
+
+#region Custom Loging System
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.Console()
+    .WriteTo.File("Logs/RealEstateLogs.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
+#endregion
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Host.UseSerilog();
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddControllers();
+builder.Services.AddControllers(options => { options.ReturnHttpNotAcceptable = true; }).AddNewtonsoftJson()
+    .AddXmlDataContractSerializerFormatters();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 #region Db Context IOC
 
